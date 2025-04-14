ee62d771bc5ffb4d3b8e7b56fbd5474ed8bf883f
JOSE ADRIAN ANDRADE GURROLA <115905103+Adri13A@users.noreply.github.com>
2025-04-13 20:47:12 -0700

    Header & Hero (#8)
    * Actualiza el componente Header y agrega soporte para modo oscuro en CSS
    
    * Agrega el paquete @heroicons/react y actualiza el componente Header con nuevo diseño y funcionalidad
    
    * Agrega configuración de DaisyUI y Tailwind CSS, elimina imagen no utilizada y actualiza el componente de la página principal, casi por terminal el componente del header
    
    * Agrega componente WeatherAPI, actualiza el Header con iconos y menú desplegable para web y movil, mejora estilos de navegación y corrige importaciones de imágenes
    
    * Elimina archivos .rmd no utilizados, mejora el componente Header con nuevos iconos y funcionalidad de menú desplegable, actualiza el componente Weather para incluir un loader, ajusta estilos en globals.css y header.css, y crea el componente Hero para la página principal.
    
    * Elimina el componente Footer de la página principal y limpia estilos en header.css y footer.css.
    
    * Mejora el componente Hero con un diseño más atractivo y ajusta el tamaño de la imagen. Comenta el componente CarouselCompanies en la página principal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-
-import Image from "next/image";
-import imagenMain from '../public/images/Mazatlan-Malecon.jpg';
+import imagenMain from '@/public/images/Mazatlan-Malecon.jpg';
+import Hero from "./components/pages/inicio/hero";
 import Footer from "./components/layout/footer";
 import CarouselCompanies from "./components/bannerCompanies/CarouselCompanies";
 import GalleryVillages from "./components/gallery/GalleryVillage";
@@ -9,47 +8,11 @@ import GalleryVillages from "./components/gallery/GalleryVillage";
 
 export default function Home() {
   return (
+    
     <div> 
 
-      <section>
-        <div className="">
-          <Image
-            src={imagenMain}
-            width={1920}
-            height={1080}
-            alt="Mazatlán"
-            className=""
-          />
-           <Image
-            src={imagenMain}
-            width={1920}
-            height={1080}
-            alt="Mazatlán"
-            className=""
-          />
-           <Image
-            src={imagenMain}
-            width={1920}
-            height={1080}
-            alt="Mazatlán"
-            className=""
-          />
-           <Image
-            src={imagenMain}
-            width={1920}
-            height={1080}
-            alt="Mazatlán"
-            className=""
-          />
-          <div className="inset-0 bg-black bg-opacity-40 flex flex-col justify-center items-center text-white">
-            <h1 className="text-5xl font-bold">Mazatlán</h1>
-            <p className="mt-2 text-lg">
-              Su voz nahuatl <span className="font-bold">Mazatlan</span> significa Tierra de Venados.
-            </p>
-          </div>
-       </div>
-      </section>
-
+      <Hero/>
+      
        {/* Gallery Villages */}
        <section>
         <div className="p-5 md:p-20 bg-gray-100">
@@ -61,11 +24,9 @@ export default function Home() {
       {/* Banner Companies Definidos en el componente pare tener el efecto */}
       <section>
         <div className="bg-gray-100">
-          <CarouselCompanies />
+          {/*<CarouselCompanies />*/}
         </div>
       </section>
-
-      <Footer />
 
     </div>
   );
