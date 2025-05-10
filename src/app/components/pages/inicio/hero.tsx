9aaabc991c34b57be4fac263034ef75857e4f7b5
JOSE ADRIAN ANDRADE GURROLA <jgurrola217@gmail.com>
2025-05-10 00:58:17 -0700

    Agregar dependencias de react-countup y countup.js; crear archivo de datos para los slides del héroe; optimizar importaciones y estilos en el componente Hero; ajustar estilos en el footer.

--- src/app/components/pages/inicio/hero.tsx
+++ src/app/components/pages/inicio/hero.tsx
@@ -2,7 +2,7 @@
 'use client'
 
 import React, { useState } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { motion, AnimatePresence, Variants } from 'framer-motion';
 
@@ -11,77 +11,9 @@ import CardHero from '../../ui/cardhero/cardhero';
 import { HeroButton } from "../../ui/buttons/herobutton";
 import Badge from "../../ui/badge/badgevertical";
 
-// Importa aquí tus imágenes de hero y mini‑card
-import heroOcean from '@/public/images/hero/heroOceano.jpg';
-import heroCarnaval from '@/public/images/hero/heroCarnaval.jpg';
-import heroRuta from '@/public/images/hero/heroRuta.jpeg';
-import heroAcuario from '@/public/images/hero/heroAcuario.jpg';
-
-import cardOcean from '@/public/images/miniCard/cardCarnaval.jpg';
-import cardCarnaval from '@/public/images/miniCard/cardOceano.jpg';
-import cardRuta from '@/public/images/miniCard/cardRuta.jpeg';
-import cardAcuario from '@/public/images/miniCard/cardAcuario.jpg';
-
-
-// Datos de cada “slide”
-const slides = [
-  {
-    id: 0,
-    heroImg: heroOcean as StaticImageData,
-    title: 'MAZATLÁN',
-    subtitle: 'Descubre la perla del pacífico',
-    link: '',
-    verticalText: 'OCEANO PACIFICO',
-    card: {
-      title: 'CULTURA',
-      text: 'Sumergete en las tradiciones, cosumbres e historia...',
-      img: cardOcean as StaticImageData
-    }
-  },
-  {
-    id: 1,
-    heroImg: heroCarnaval as StaticImageData,
-    title: 'CULTURA',
-    subtitle: 'Sumergete en las tradiciones, cosumbres e historia',
-    link: 'https://mazatlan.travel/',
-    verticalText: 'CARNAVAL MAZATLÁN 2024',
-    card: {
-      title: 'RUTAS',
-      text: 'Conoce las diferentes rutas que recorren el puerto...',
-      img: cardRuta as StaticImageData
-    }
-  },
-  {
-    id: 2,
-    heroImg: heroRuta as StaticImageData,
-    title: 'RUTAS',
-    subtitle: 'Conoce las diferentes rutas que recorren el puerto...',
-    link: 'https://mazatlan.travel/',
-    verticalText: 'RUTA SABALO',
-    card: {
-      title: 'ATRACCIONES',
-      text: 'Descubre los lugares más emblemáticos de Mazatlán...',
-      img: cardAcuario as StaticImageData
-    }
-  },
-  {
-    id: 3,
-    heroImg: heroAcuario as StaticImageData,
-    title: 'DESTINOS',
-    subtitle: 'Descubre los lugares más emblemáticos de Mazatlán',
-    link: 'https://mazatlan.travel/',
-    verticalText: 'ACUARIO MAR DE CORTES',
-    card: {
-      title: 'MAZATLÁN',
-      text: 'Descubre la perla del pacífico...',
-      img: cardCarnaval as StaticImageData
-    }
-  },
-
-];
+import { slides } from "../../../data/slides"; // Importa los datos de los slides
 
 // Animations
-
 const container: Variants = {
   hidden: { opacity: 0 },
   show: { opacity: 1, transition: { staggerChildren: 0.2, when: 'beforeChildren' } }
